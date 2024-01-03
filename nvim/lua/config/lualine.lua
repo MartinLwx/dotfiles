6c07b56be3b7be3b7d6cf5d3270fd7a61008acdf
MartinLwx <MartinLwx@163.com>
2024-01-04 00:19:48 +0800

    🔧 [Nvim] show absolute path and filesize in Lualine

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,48 +1,67 @@
-local is_ok, lualine = pcall(require, 'lualine')
+local is_ok, lualine = pcall(require, "lualine")
 if not is_ok then
-    return
+	return
 end
 
-lualine.setup {
-    options = {
-        icons_enabled = true,
-        theme = 'material',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        }
-    },
-    -- +-------------------------------------------------+
-    -- | A | B | C                             X | Y | Z |
-    -- +-------------------------------------------------+
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
-}
+lualine.setup({
+	options = {
+		icons_enabled = true,
+		theme = "material",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		},
+	},
+	-- Lualine has sections as shown below.
+	-- +-------------------------------------------------+
+	-- | A | B | C                             X | Y | Z |
+	-- +-------------------------------------------------+
+	-- Each sections holds its components
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = {
+			"branch",
+			"diff",
+			"diagnostics",
+		},
+		lualine_c = {
+			{
+				"filename",
+				file_status = true, -- Displays file status (readonly status, modified status)
+				-- Path configurations
+				-- 0: Just the filename
+				-- 1: Relative path
+				-- 2: Absolute path
+				-- 3: Absolute path, with tilde as the home directory
+				-- 4: Filename and parent dir, with tilde as the home directory
+				path = 3,
+				shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+			},
+		},
+		lualine_x = { "encoding", "filesize", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {},
+})
