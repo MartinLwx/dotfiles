d115bee0e560fe002812e7c638d14709eff84dfe
MartinLwx <MartinLwx@163.com>
2024-01-23 14:53:00 +0800

    🔧 [Nvim] OCaml LSP & Formatter setup

--- nvim/lua/config/mason-null-ls.lua
+++ nvim/lua/config/mason-null-ls.lua
@@ -3,29 +3,19 @@ if not mason_ok then
 	return
 end
 
-mason.setup()
-
 local null_ls_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_ok then
 	return
 end
 
-local sources = {
-	null_ls.builtins.formatting.black.with({ extra_args = { "--target-version", "py310" } }),
-	null_ls.builtins.formatting.stylua,
-}
-
-null_ls.setup({
-	debug = false,
-	log_level = "warn",
-	update_in_insert = false,
-	sources = sources,
-})
-
 local mason_null_ls_ok, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_ok then
 	return
 end
+
+-- Hint:
+
+mason.setup()
 
 mason_null_ls.setup({
 	-- A list of sources to install if they're not already installed.
@@ -34,8 +24,34 @@ mason_null_ls.setup({
 		"black",
 		"stylua",
 	},
+	-- Run `require("null-ls").setup`.
+	-- Will automatically install masons tools based on selected sources in `null-ls`.
+	-- Can also be an exclusion list.
+	-- Example: `automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }`
 	automatic_installation = false,
 	-- Sources found installed in mason will automatically be setup for null-ls.
 	automatic_setup = true,
-	handlers = {},
+	handlers = {
+		-- Hint: see https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTIN_CONFIG.md
+		--       to see what sources are available
+		-- Hint: see https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTIN_CONFIG.md
+		--       to check what we can configure for each source
+		-- function() end, -- disables automatic setup of all null-ls sources
+		black = function(source_name, methods)
+			null_ls.register(null_ls.builtins.formatting.black)
+		end,
+		stylua = function(source_name, methods)
+			null_ls.register(null_ls.builtins.formatting.stylua)
+		end,
+		ocamlformat = function(source_name, methods)
+			null_ls.register(null_ls.builtins.formatting.ocamlformat.with({
+				-- Add more arguments to a source's defaults
+				-- Default: { "--enable-outside-detected-project", "--name", "$FILENAME", "-" }
+				-- Type `ocamlformat --help` in your terminal to check more args
+				extra_args = { "--if-then-else", "vertical" },
+			}))
+		end,
+	},
 })
+
+null_ls.setup()
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,65 +1,65 @@
 -- Note: The order matters: mason -> mason-lspconfig -> lspconfig
-require('mason').setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
-require('mason-lspconfig').setup({
-    -- A list of servers to automatically install if they're not already installed
-    ensure_installed = { 'pylsp', 'gopls', 'lua_ls', 'bashls', 'rust_analyzer' },
+require("mason-lspconfig").setup({
+	-- A list of servers to automatically install if they're not already installed
+	ensure_installed = { "pylsp", "gopls", "lua_ls", "bashls", "rust_analyzer" },
 })
-
 
 -- Set different settings for different languages' LSP
 -- LSP list: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 -- How to use setup({}): https://github.com/neovim/nvim-lspconfig/wiki/Understanding-setup-%7B%7D
 --     - the settings table is sent to the LSP
 --     - on_attach: a lua callback function to run after LSP attaches to a given buffer
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
 -- Customized on_attach function
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f', function()
-        vim.lsp.buf.format({
-            async = true,
-            filter = function (client)
-                return client.name == "null-ls"
-            end
-        })
-    end, bufopts)
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	vim.keymap.set("n", "<space>f", function()
+		vim.lsp.buf.format({
+			async = true,
+			-- Only request null-ls for formatting
+			filter = function(client)
+				return client.name == "null-ls"
+			end,
+		})
+	end, bufopts)
 end
 
 -- Configure each language
@@ -67,51 +67,55 @@ end
 -- 1. use `:Mason` to install corresponding LSP
 -- 2. add configuration below
 lspconfig.pylsp.setup({
-    on_attach = on_attach,
+	on_attach = on_attach,
 })
 
 lspconfig.gopls.setup({
-    on_attach = on_attach,
+	on_attach = on_attach,
 })
 
-lspconfig.lua_ls.setup {
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { 'vim' },
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-}
+lspconfig.lua_ls.setup({
+	on_attach = on_attach,
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
 
 lspconfig.bashls.setup({})
 
 -- source: https://rust-analyzer.github.io/manual.html#nvim-lsp
 lspconfig.rust_analyzer.setup({
-    on_attach = on_attach,
-    settings = {
-        ['rust-analyzer'] = {
-            inlayHints = {
-                closingBraceHints = true, -- Whether to show inlay hints after a closing } to indicate what item it belongs to.
-            }
-        }
-    }
+	on_attach = on_attach,
+	settings = {
+		["rust-analyzer"] = {
+			inlayHints = {
+				closingBraceHints = true, -- Whether to show inlay hints after a closing } to indicate what item it belongs to.
+			},
+		},
+	},
 })
 
 lspconfig.clangd.setup({
-    on_attach = on_attach,
+	on_attach = on_attach,
+})
+
+lspconfig.ocamllsp.setup({
+	on_attach = on_attach,
 })
