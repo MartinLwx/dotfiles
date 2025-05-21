f2f74493990877c58a0460b6e3eb289dce31384a
MartinLwx <MartinLwx@163.com>
2025-05-22 00:07:37 +0800

    🔧 [Nvim] config: showing diagnostic when cursor hover for 4 seconds

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -8,18 +8,26 @@ vim.keymap.del("n", "gO")
 -- Create keymapping
 -- LspAttach: After an LSP Client performs "initialize" and attaches to a buffer.
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function (args)
-        local keymap = vim.keymap
-        local lsp = vim.lsp
-	    local bufopts = { noremap = true, silent = true }
+	callback = function(args)
+		local keymap = vim.keymap
+		local lsp = vim.lsp
+		local bufopts = { noremap = true, silent = true }
 
-        keymap.set("n", "gr", lsp.buf.references, bufopts)
-        keymap.set("n", "gd", lsp.buf.definition, bufopts)
-        keymap.set("n", "<space>rn", lsp.buf.rename, bufopts)
-        keymap.set("n", "K", lsp.buf.hover, bufopts)
-        keymap.set("n", "<space>f", function()
-            require("conform").format({ async = true, lsp_fallback = true })
-        end, bufopts)
-    end
+		keymap.set("n", "gr", lsp.buf.references, bufopts)
+		keymap.set("n", "gd", lsp.buf.definition, bufopts)
+		keymap.set("n", "<space>rn", lsp.buf.rename, bufopts)
+		keymap.set("n", "K", lsp.buf.hover, bufopts)
+		keymap.set("n", "<space>f", function()
+			require("conform").format({ async = true, lsp_fallback = true })
+		end, bufopts)
+	end,
 })
 
+-- CursorHold: When the user doesn't press a key for the time specified with 'updatetime'
+--             By default, `updatetime` is equal to 4000 ms
+--
+vim.api.nvim_create_autocmd("CursorHold", {
+	callback = function()
+		vim.diagnostic.open_float(nil, { focusable = false, source = "if_many" })
+	end,
+})
