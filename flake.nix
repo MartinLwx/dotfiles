bfa95a1408d628fe8d7d9e246d283077709b54bd
MartinLwx <MartinLwx@163.com>
2025-08-17 21:22:10 +0800

    🔧 [Nix] deps: introduce home-manager

--- flake.nix
+++ flake.nix
@@ -7,13 +7,27 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   # A Nix function whose parameters come from inputs.
-  outputs = inputs@{ self, nix-darwin, nixpkgs, ... }:
+  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager, ... }:
   let
     # See the full manual here: https://nix-darwin.github.io/nix-darwin/manual/
+    
+    # TODO: Replace martinlwx with your name.
+    username = "martinlwx";
     configuration = { pkgs, ... }: {
+      # WARN: To make home-manager work as expected, you need to define user info here.
+      # See: https://github.com/nix-community/home-manager/issues/6036
+      users = {
+        users.${username} = {
+	  home = "/Users/${username}";
+	  name = "${username}";
+	};
+      };
+
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
@@ -28,7 +42,14 @@
 	  pkgs.fd        # Better find
 	  pkgs.uv        # Python environment manager
 	  pkgs.autojump  # Cd command that learns
+	  # pkgs.neo4j     # Graph DB
+	  # === macOS  ===
         ];
+
+      # Envionrment Variables
+      environment.variables = {
+        EDITOR = "nvim";
+      };
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
@@ -45,11 +66,13 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+
       # Enable Touch ID and Apple Watch with sudo.
       security.pam.services.sudo_local.touchIdAuth = true;
 
       # macOS system settings
-      system.primaryUser = "MartinLwx";
+      # TODO: Replace martinlwx with your name.
+      system.primaryUser = "martinlwx";
       system.defaults = {
         # Menu bar settings.
 	controlcenter = {
@@ -73,7 +96,25 @@
     #      e.g., nix-darwin.lib.darwinSystem
     # TIP: Build darwin flake using: $ sudo darwin-rebuild build --flake .#mba
     darwinConfigurations."mba" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        configuration 
+	# Ensure that the home-manager will be rebuilt with the nix-darwin generations.
+        home-manager.darwinModules.home-manager
+        {
+	  home-manager = {
+	    # Use the global pkgs that is configured via the system level nixpkgs options.
+            useGlobalPkgs = true;
+            useUserPackages = true;
+      	    # TODO: Replace martinlwx with your name.
+	    # WARN: Add home.nix to the git to avoid error like Path '.../home.nix' does not exists.
+	    #       See: https://discourse.nixos.org/t/flake-based-home-manager-cannot-find-home-nix/18356
+            users.martinlwx = import ./home.nix;
+	  };
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }	
+      ];
     };
   };
 }
---
+++ home.nix
@@ -0,0 +1,26 @@
+# NOTE: The manual: https://nix-community.github.io/home-manager/
+{ config, pkgs, ... }:
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "martinlwx";
+
+  # # Packages that should be installed to the user profile.
+  # home.packages = [
+  #   pkgs.neo4j   # Graph DB
+  # ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+}
