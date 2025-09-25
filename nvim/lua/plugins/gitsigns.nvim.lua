fda1027f055a29f560ae366c0703ac7063fb41bb
MartinLwx <MartinLwx@163.com>
2025-09-25 23:53:53 +0800

    ✨ Feat(nvim): add support for preview-hunk with <leader>gp

--- nvim/lua/plugins/gitsigns.nvim.lua
+++ nvim/lua/plugins/gitsigns.nvim.lua
@@ -1,6 +1,8 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
-		require("config.gitsigns")
+		require("gitsigns").setup()
+
+		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
 	end,
 }
